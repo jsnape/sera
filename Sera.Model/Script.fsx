4db29ab7e6fb2c8427f4ca6c93af78b8965ddde0
James Snape <james.snape@gmail.com>
2017-03-15 13:58:13 +0000

    Diagrams

--- Sera.Model/Script.fsx
+++ Sera.Model/Script.fsx
@@ -1,11 +1,58 @@
 ﻿
 #load "Library1.fs"
+
 open Sera.Model
 
+open System
+
+type Amount = decimal
+type Payee = string
+type Count = int
+type Level = float
+
+type Knowledge =
+    | Financial of DateTime * Payee * Amount
+    | Steps of DateTime * Count
+    | Mood of DateTime * Level
+
 type Blackboard = {
-    Name: string
+    Knowledge: Knowledge list
     }
+
+type BlackboardUpdate = Blackboard -> Blackboard
 
 type KnowledgeSource = {
     Name: string
+    UpdateBlackboard: BlackboardUpdate
     }
+
+let tran1 = Financial(DateTime.Today, "Starbucks", 5.32M)
+
+let EventLoop (blackboard : Blackboard) (sources : KnowledgeSource list) : Blackboard =
+    let UpdateBoard blackboard source =
+        source.UpdateBlackboard blackboard 
+
+    List.fold UpdateBoard blackboard sources
+
+/////////////////////////////////////////////////////////
+
+type Location = float * float
+
+type TriggerSource = 
+    | Button
+    | DateTime of DateTime
+    | Location of Location
+    | Transaction of DateTime * Payee * Amount
+
+type Trigger = {
+    Label: string
+    Signalled: bool
+    Source: TriggerSource
+    }
+
+type Action = {
+    Label: string
+    }
+
+type ActionPlan = (Trigger * Action) list
+
